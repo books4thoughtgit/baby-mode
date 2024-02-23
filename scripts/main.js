390ddc712304a4dbacb34b326f5309c2fdb3153a
books4thoughtgit <158254928+books4thoughtgit@users.noreply.github.com>
2024-02-23 13:35:39 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,4 @@ require('./content/blocks');
 
 Events.on(WorldLoadEvent, e => {
     Vars.state.rules.infiniteResources = true
-    Vars.state.rules.instantBuild = true
-    Vars.state.rules.waves = true
 });
